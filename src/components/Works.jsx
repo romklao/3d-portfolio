bf45b6d354defe98cfd154420e14f7e5b891eb4b
Romklao Chainuwong <rk120879@gmail.com>
2025-06-02 21:57:48 -0700

    rewrote description

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -90,16 +90,15 @@ const Works = () => {
     <>
       <motion.div>
         <p className={`${styles.sectionSubText}`}>My work</p>
-        <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
+        <h2 className={`${styles.sectionHeadText}`}>Tutorial Projects.</h2>
       </motion.div>
 
       <div className="w-full flex">
         <motion.p className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]">
-          Following projects showcases my skills and experience through
-          real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in it. It reflects my
-          ability to solve complex problems, work with different technologies,
-          and manage projects effectively.
+          These tutorial projects are hands-on learning exercises that help me
+          explore and understand modern web development tools and frameworks.
+          Each project focuses on a specific technology or stack, allowing me to
+          build practical skills by following along with real-world tutorials.
         </motion.p>
       </div>
 
